f2ce7b97b23d326ab8c8f4d9f3d93284960292c7
Kamil Jabłoński <Jaboleo@users.noreply.github.com>
2022-07-02 16:02:37 +0200

    Revert "Feature/authentication"

--- family_budget/urls.py
+++ family_budget/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("users/", include("users.urls"), name="users"),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
 ]

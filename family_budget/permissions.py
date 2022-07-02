35547d81aea16ed5e49d9e2bdb9f2ea7282e4353
Kamil <kljablon@gmail.com>
2022-07-02 18:03:05 +0200

    fixed auth permissions

---
+++ family_budget/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrAdmin(permissions.BasePermission):
+    """
+    Object-level permission to only allow authors of an object and admins to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user or request.user.is_staff is True
+
+class IsThisUserOrAdmin(permissions.BasePermission):
+    """
+    Object-level permission to only allow authors of an object and admins to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user or request.user.is_staff is True
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 
+from family_budget.permissions import IsThisUserOrAdmin
 from users.models import User
 from users.serializers import UserSerializer
 
@@ -11,6 +13,14 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
 
     def get_permissions(self):
+        if self.action == "create":
+            return (IsAuthenticated(),)
         if self.action in ["destroy", "update", "partial_update"]:
-            return IsAuthenticated()
+            return (IsThisUserOrAdmin(),)
         return (AllowAny(),)
+
+    def destroy(self, request, pk=None):
+        user_to_delete = User.objects.get(pk=pk)
+        self.check_object_permissions(self.request, user_to_delete)
+        user_to_delete.delete()
+        return Response(f"User with id {pk} successfully deleted!")
